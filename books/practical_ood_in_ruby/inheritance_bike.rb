c870451918d94d22c0ada07ab1bce5faa7f79dd4
wyattsweet <wyattsweet@gmail.com>
2018-06-30 23:02:01 -0700

    POODR - ch: 6 subclassed mountain bike

--- books/practical_ood_in_ruby/inheritance_bike.rb
+++ books/practical_ood_in_ruby/inheritance_bike.rb
@@ -1,36 +1,24 @@
-class Bike
+class Bicycle
   attr_reader :size, :tape_color
 
   def initialize(args)
-    @style = args[:size]
-    @size = args[:size]
-    @tape_color = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = argsp[:rear_shock]
+    @size        = args[:size]
+    @tape_color  = args[:tape_color]
   end
 
   def spares
-    if style == :road
-      {chain: '10-speed',
-       tire_size: '12',
-       tape_color: tape_color
-      }
-    else
-      {
-        chain: '10-speed',
-        tire_size: '2.1',
-        rear_shocks: rear_shocks
-      }
-    end
+    { chain:        '10-speed',
+      tire_size:    '23',       # milimeters
+      tape_color:   tape_color }
   end
 end
 
-class MountainBike < Bike
+class MountainBike < Bicycle
   attr_reader :front_shock, :rear_shock
 
-  def initialize
+  def initialize(args)
     @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
+    @rear_shock  = args[:rear_shock]
     super(args)
   end
 
@@ -39,10 +27,10 @@ class MountainBike < Bike
   end
 end
 
-bike = Bike.new(
-  size: 'M',
-  tape_color: 'red'
-)
+mountain_bike = MountainBike.new(
+                  size:         'S',
+                  front_shock:  'Manitou',
+                  rear_shock:   'Fox')
 
-puts bike.size
-puts bike.spares
+puts mountain_bike.size
+puts mountain_bike.spares
