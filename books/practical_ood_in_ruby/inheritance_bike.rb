0f7cf43c352ce64a1b31609106e7a35b7f180190
wyattsweet <wyattsweet@gmail.com>
2018-06-30 22:28:15 -0700

    POODR - ch: 6 added bike style to Boke object

--- books/practical_ood_in_ruby/inheritance_bike.rb
+++ books/practical_ood_in_ruby/inheritance_bike.rb
@@ -2,16 +2,26 @@ class Bike
   attr_reader :size, :tape_color
 
   def initialize(args)
+    @style = args[:size]
     @size = args[:size]
     @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = argsp[:rear_shock]
   end
 
   def spares
-    {
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      {chain: '10-speed',
+       tire_size: '12',
+       tape_color: tape_color
+      }
+    else
+      {
+        chain: '10-speed',
+        tire_size: '2.1',
+        rear_shocks: rear_shocks
+      }
+    end
   end
 end
 
